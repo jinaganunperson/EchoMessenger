1bf29225bcdb63edb4dd09a87cd76ecb7b2c15da
jinaganunperson <06younseo@gmail.com>
2026-03-24 20:21:30 +0900

    과제1 코딩과 테스트 완료 라벨, 텍스트박스, 버튼, 리스트 박스를 이용해 ui를 구성하였음. 전송 버튼 클릭 시 텍스트박스의 텍스트를 리스트박스의 항목으로 추가함. 추가 직후 텍스트박스의 내용을 비워 다음 입력을 준비함.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnInput = new Button();
+            lbl = new Label();
+            txtBox = new TextBox();
+            listBox = new ListBox();
             SuspendLayout();
+            // 
+            // btnInput
+            // 
+            btnInput.Location = new Point(821, 493);
+            btnInput.Name = "btnInput";
+            btnInput.Size = new Size(132, 58);
+            btnInput.TabIndex = 0;
+            btnInput.Text = "전송";
+            btnInput.UseVisualStyleBackColor = true;
+            btnInput.Click += btnInput_Click;
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Font = new Font("휴먼편지체", 28.125F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 129);
+            lbl.Location = new Point(242, 25);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(513, 87);
+            lbl.TabIndex = 1;
+            lbl.Text = "EchoMessenger";
+            // 
+            // txtBox
+            // 
+            txtBox.Location = new Point(97, 494);
+            txtBox.Name = "txtBox";
+            txtBox.Size = new Size(711, 42);
+            txtBox.TabIndex = 2;
+            // 
+            // listBox
+            // 
+            listBox.FormattingEnabled = true;
+            listBox.Location = new Point(59, 116);
+            listBox.Name = "listBox";
+            listBox.Size = new Size(802, 328);
+            listBox.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(15F, 36F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1053, 620);
+            Controls.Add(listBox);
+            Controls.Add(txtBox);
+            Controls.Add(lbl);
+            Controls.Add(btnInput);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnInput;
+        private Label lbl;
+        private TextBox txtBox;
+        private ListBox listBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,26 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            // 1. 텍스트박스의 내용 가져오기
+            string inputBox = txtBox.Text;
+
+            // 2. 입력값이 비어있지 않은지 확인
+            if (!string.IsNullOrWhiteSpace(inputBox))
+            {
+                // 리스트박스에 항목 추가
+                listBox.Items.Add(inputBox);
+
+                // 3. 입력 후 텍스트박스 비우기 및 포커스 이동
+                txtBox.Clear();
+                txtBox.Focus();
+            }
+            else
+            {
+                MessageBox.Show("내용을 입력해주세요!");
+            }
+        }
     }
 }
